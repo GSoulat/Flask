78c1ab8bcf4cb16ea87dc15ce91c6f9449fd42c6
gsoulat <gsoulat31@gmail.com>
2022-06-23 14:35:02 +0100

    update

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 from App import app
-# from flask_socketio import SocketIO, send
 import os
-
-# socketio = SocketIO(app, cors_allowed_origins='*')
-
-# @socketio.on('message')
-# def handle_message(message):
-#     print("Received messsage: " + message)
-#     if message != "User connected":
-#         send(message, broadcast=True)
-
-
 
 if __name__ == '__main__':
     # os.environ['FLASK_ENV'] = 'development'
