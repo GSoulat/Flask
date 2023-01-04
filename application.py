bda90d5b8afed6f68e4f0263011bdc155e1e384e
gsoulat <gsoulat31@gmail.com>
2023-01-04 09:51:33 +0100

    update

--- application.py
+++ application.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
     
     # os.environ['FLASK_ENV'] = 'production'
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0',port=80)
     
     
